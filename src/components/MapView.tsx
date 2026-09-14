9cb4b07b6459820d8762fefd9f202f1aaaddfb23
strada-jeff <jsylvester@stradamade.com>
2026-09-14 11:32:04 -0600

    updating base map size

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,31 +1,31 @@
-import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet'
-import L, { type LatLngBoundsExpression } from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
-import markerIcon from 'leaflet/dist/images/marker-icon.png'
-import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import { MapContainer, ImageOverlay, Marker, Popup } from "react-leaflet";
+import L, { type LatLngBoundsExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
   iconUrl: markerIcon,
   shadowUrl: markerShadow,
-})
+});
 
-const BASE_MAP_WIDTH = 3840
-const BASE_MAP_HEIGHT = 2160
+const BASE_MAP_WIDTH = 3840;
+const BASE_MAP_HEIGHT = 2160;
 const BASE_MAP_BOUNDS: LatLngBoundsExpression = [
   [0, 0],
   [BASE_MAP_HEIGHT, BASE_MAP_WIDTH],
-]
+];
 const BASE_MAP_CENTER: [number, number] = [
   BASE_MAP_HEIGHT / 2,
   BASE_MAP_WIDTH / 2,
-]
+];
 
 function MapView() {
   return (
     <MapContainer
-      className="h-full w-full"
+      className="h-full w-full bg-[#f2f0ee]!"
       crs={L.CRS.Simple}
       bounds={BASE_MAP_BOUNDS}
       maxBounds={BASE_MAP_BOUNDS}
@@ -33,12 +33,12 @@ function MapView() {
       maxZoom={2}
       scrollWheelZoom
     >
-      <ImageOverlay url="/base.svg" bounds={BASE_MAP_BOUNDS} />
+      <ImageOverlay url="/base.png" bounds={BASE_MAP_BOUNDS} />
       <Marker position={BASE_MAP_CENTER}>
         <Popup>Center of base map</Popup>
       </Marker>
     </MapContainer>
-  )
+  );
 }
 
-export default MapView
+export default MapView;
